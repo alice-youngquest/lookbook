a843f0441821ccd2458c60a5fc43b54f693e2d80
jon-atto-bennett <jon-atto-bennett>
2017-04-13 19:03:42 +1200

    errors errors errors

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import PhotoList from './PhotoList'
 import WeatherContainer from '../containers/WeatherContainer'
 import Nav from './Nav'
@@ -8,8 +13,13 @@ const Home = () => {
     <div>
       <Nav />
       <p>Home Test</p>
-      <PhotoList />
-      <WeatherContainer />
+      <Router>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route path='/photo-list' component={PhotoList} />
+      </div>
+    </Router>
+    <WeatherContainer />
     </div>
   )
 }
--- client/components/PhotoList.jsx
+++ client/components/PhotoList.jsx
@@ -2,46 +2,46 @@ import React from 'react'
 import PhotoListItem from './PhotoListItem'
 import { listAllOutfits } from '../api'
 
-const PhotoList = () => {
-  return (
-    <div>
-      <p>PhotoList Test</p>
-      <PhotoListItem />
-    </div>
-  )
-}
+// const PhotoList = () => {
+//   return (
+//     <div>
+//       <p>PhotoList Test</p>
+//       <PhotoListItem />
+//     </div>
+//   )
+// }
 
-// export default React.createClass({
-//   getInitialState () {
-//     return {
-//       allOutfits: {
-//         imageUrl: "https://avatars1.githubusercontent.com/u/23620176?v=3&s=200",
-//         name: "Kakapo",
-//         outfits: []
-//       }
-//     }
-//   },
-//
-//   componentDidMount () {
-//     getTeam((err, allOutfits) => {
-//       if (err) return console.log(err)
-//       this.setState({ allOutfits })
-//     })
-//   },
-//
-//   render () {
-//     return (
-//       <div className="Home">
-//         {this.state.allOutfits.outfits.map((outfit) => {
-//           return (
-//           <Status outfit={outfit} key={outfit.id}/>
-//           )
-//         })}
-//         <p>PhotoList Test</p>
-//         <PhotoListItem />
-//       </div>
-//     )
-//   }
-// })
+export default React.createClass({
+  getInitialState () {
+    return {
+      allOutfits: {
+        imageUrl: "https://avatars1.githubusercontent.com/u/23620176?v=3&s=200",
+        name: "Kakapo",
+        outfits: []
+      }
+    }
+  },
 
-module.exports = PhotoList
+  componentDidMount () {
+    listAllOutfits((err, allOutfits) => {
+      if (err) return console.log(err)
+      this.setState({ allOutfits })
+    })
+  },
+
+  render () {
+    return (
+      <div className="Home">
+        {this.state.allOutfits.outfits.map((outfit) => {
+          return (
+          <Status outfit={outfit} key={outfit.id}/>
+          )
+        })}
+        <p>PhotoList Test</p>
+        <PhotoListItem />
+      </div>
+    )
+  }
+})
+
+// module.exports = PhotoList
