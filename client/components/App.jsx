bb1e9b0d53749623f44c5b5aa128807cd60296b7
Alice Margaret Youngquest <alice_youngquest@Hotmail.com>
2017-04-18 11:23:53 +1200

    fixed

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { HashRouter as Router, Route} from 'react-router-dom'
 
 import Weather from '../containers/Weather'
-<<<<<<< HEAD
 import Home from './Home'
 
 const App = () => (
@@ -13,22 +12,5 @@ const App = () => (
         <Route path='/inspo' component={Weather}/>
       </div>
     </Router>
-=======
-import OutfitsList from '../containers/OutfitsList'
-import SearchByTag from '../containers/SearchByTag'
-
-const App = () => (
-  <div className='app-container'>
-    <Weather />
-    <SearchByTag />
-    <OutfitsList />
->>>>>>> cc7f4199b3ea2b967cd02cae0f85991e94d04348
-  </div>
-)
 
 export default App
-
-
-
-// <Route path='/outfits' component={OutfitsList}/>
-// import OutfitsList from '../containers/OutfitsList'
