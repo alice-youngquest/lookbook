5745cf09aa971b06914ab2a6b54017f8f3a78ea7
Alice Margaret Youngquest <alice_youngquest@Hotmail.com>
2017-04-17 12:44:07 +1200

    pics

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 import Weather from '../containers/Weather'
 import OutfitsList from '../containers/OutfitsList'
+import Home from '../components/Home'
 
 const App = () => (
   <div className='app-container'>
-    <Weather />
-    <OutfitsList />
+    <Home />
   </div>
 )
 
--- public/bundle.js
+++ public/bundle.js
@@ -23919,14 +23919,17 @@
 	
 	var _OutfitsList2 = _interopRequireDefault(_OutfitsList);
 	
+	var _Home = __webpack_require__(231);
+	
+	var _Home2 = _interopRequireDefault(_Home);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	var App = function App() {
 	  return _react2.default.createElement(
 	    'div',
 	    { className: 'app-container' },
-	    _react2.default.createElement(_Weather2.default, null),
-	    _react2.default.createElement(_OutfitsList2.default, null)
+	    _react2.default.createElement(_Home2.default, null)
 	  );
 	};
 	
@@ -25835,6 +25838,33 @@
 	    likes: likes
 	  };
 	};
+
+/***/ },
+/* 231 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _react = __webpack_require__(1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var Home = function Home() {
+	  return _react2.default.createElement(
+	    "div",
+	    null,
+	    _react2.default.createElement("div", { id: "circle" })
+	  );
+	};
+	
+	exports.default = Home;
+	module.exports = exports["default"];
 
 /***/ }
 /******/ ]);
